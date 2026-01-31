1f7a7bacff9c02188dfa3ddc93fe5497f8449108
jdelvalle-ia <juliandelvallefernandez@gmail.com>
2026-01-31 20:20:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,9 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       // Priorizamos GEMINI_API_KEY (de tu Netlify) y la mapeamos a la variable que espera el código
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || env.API_KEY || '')
+      //'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || env.API_KEY || '')
+      'process.env': {
+        API_KEY: JSON.stringify(env.GEMINI_API_KEY || env.API_KEY || ''),
     },
     resolve: {
       alias: {
