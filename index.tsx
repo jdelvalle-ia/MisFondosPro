0c1647f8d1c3c7043131e5cd75252291511ab90e
jdelvalle-ia <juliandelvallefernandez@gmail.com>
2026-01-31 18:47:34 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+window.process = { env: { API_KEY: import.meta.env.VITE_GEMINI_API_KEY } };
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
